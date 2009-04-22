237a150297c8324192d6c3134a4a1cfeedf01b55
roberto@sirius <roberto@sirius>
2009-04-22 09:29:36 +0200

    fix script_name and path_info

--- mod_uwsgi.c
+++ mod_uwsgi.c
@@ -1,16 +1,31 @@
-// apxs2 -i -c mod_uwsgi.c
+/* 
+        
+    *** uWSGI ***
 
-#include "ap_config.h"
-#include "apr_version.h"
-#include "apr_lib.h"
+    Copyright 2009 Unbit S.a.s.
+        
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+To compile:
+	apxs2 -i -c mod_uwsgi.c
+
+*/
+
 #include "apr_strings.h"
 #include "httpd.h"
 #include "http_config.h"
-#include "http_core.h"
-#include "http_request.h"
-#include "http_log.h"
-#include "http_protocol.h"
-#include "util_script.h"
+
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/uio.h>
