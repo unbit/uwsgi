fadee3242f58b6aab07b5a3be57f60f1f5573ad2
roberto@goyle <roberto@goyle>
2010-12-12 18:22:30 +0100

    LuaJit works perfectly and FAAAAST

--- plugins/lua/uwsgiplugin.py
+++ plugins/lua/uwsgiplugin.py
@@ -3,10 +3,20 @@ import os,sys
 try:
         LUALIB = os.environ['UWSGICONFIG_LUALIB']
 except:
-        LUALIB = 'lua5.1'
+        LUALIB = 'lua'
+
+try:
+        LUAINC = os.environ['UWSGICONFIG_LUAINC']
+except:
+        LUAINC = '/usr/include/lua5.1'
+
+try:
+        LUALIBPATH = os.environ['UWSGICONFIG_LUALIBPATH']
+except:
+        LUALIBPATH = '/usr/lib/lua5.1'
 
 NAME='lua'
-CFLAGS = ['-I/usr/include/lua5.1/']
-LDFLAGS = []
+CFLAGS = ['-I%s' % LUAINC]
+LDFLAGS = ['-L%s' % LUALIBPATH]
 GCC_LIST = ['lua_plugin']
 LIBS = ['-l%s' % LUALIB]
