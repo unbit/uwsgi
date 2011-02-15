53ac53167b8791920864c405c3e63172b8fef14a
roberto@goyle <roberto@goyle>
2011-02-15 08:39:42 +0100

    osx rwlock fix

--- lock.c
+++ lock.c
@@ -104,10 +104,10 @@ void uwsgi_unlock(void *lock) {
 #define UWSGI_LOCK_SIZE		sizeof(OSSpinLock)
 #define UWSGI_RWLOCK_SIZE	sizeof(OSSpinLock)
 
-#define uwsgi_rwlock_init uwsgi_lock_init
-#define uwsgi_rlock uwsgi_lock
-#define uwsgi_wlock uwsgi_lock
-#define uwsgi_rwunlock uwsgi_lock
+void uwsgi_rwlock_init(void *lock) { uwsgi_lock_init(lock) ;}
+void uwsgi_rlock(void *lock) { uwsgi_lock(lock);}
+void uwsgi_wlock(void *lock) { uwsgi_lock(lock);}
+void uwsgi_rwunlock(void *lock) { uwsgi_unlock(lock); }
 
 void uwsgi_lock_init(void *lock) {
 
