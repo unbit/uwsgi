05be07aab434337364d83ade5791e96e3fd4de44
roberto@cable <roberto@cable>
2011-02-23 10:29:01 +0100

    removed debug from real gil

--- plugins/python/gil.c
+++ plugins/python/gil.c
@@ -4,13 +4,11 @@ extern struct uwsgi_server uwsgi;
 extern struct uwsgi_python up;
 
 void gil_real_get() {
-	uwsgi_log("*** REAL GIL ***\n");
 	PyEval_AcquireLock();
 	PyThreadState_Swap((PyThreadState *) pthread_getspecific(up.upt_gil_key));
 }
 
 void gil_real_release() {
-	uwsgi_log("*** REAL RELEASE ***\n");
 	pthread_setspecific(up.upt_gil_key, (void *) PyThreadState_Swap(NULL));
 	PyEval_ReleaseLock();
 }
