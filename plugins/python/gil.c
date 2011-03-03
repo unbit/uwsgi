b867b11de313ea41e0b4e0cfef22be03897c1ca6
roberto@maverick64 <roberto@maverick64>
2011-03-03 17:17:27 +0100

    fixed python2.x threads

--- plugins/python/gil.c
+++ plugins/python/gil.c
@@ -4,20 +4,22 @@ extern struct uwsgi_server uwsgi;
 extern struct uwsgi_python up;
 
 void gil_real_get() {
-	/*
+#ifndef PYTHREE
 	PyEval_AcquireLock();
 	PyThreadState_Swap((PyThreadState *) pthread_getspecific(up.upt_gil_key));
-	*/
+#else
 	PyEval_RestoreThread((PyThreadState *) pthread_getspecific(up.upt_gil_key));
+#endif
 }
 
 void gil_real_release() {
-	/*
+#ifndef PYTHREE
 	pthread_setspecific(up.upt_gil_key, (void *) PyThreadState_Swap(NULL));
 	PyEval_ReleaseLock();	
-	*/
+#else
 	pthread_setspecific(up.upt_gil_key, (void *) PyThreadState_Get());
 	PyEval_SaveThread();
+#endif
 }
 
 void gil_fake_get() {}
