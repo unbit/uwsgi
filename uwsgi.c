6eed48b28daad8e89ebdc03e3b55870501f16ae0
roberto@natty32 <roberto@natty32>
2011-04-06 09:29:46 +0200

    updated Changelog and copyright year

--- uwsgi.c
+++ uwsgi.c
@@ -2,7 +2,7 @@
 
  *** uWSGI ***
 
- Copyright (C) 2009-2010 Unbit S.a.s. <info@unbit.it>
+ Copyright (C) 2009-2011 Unbit S.a.s. <info@unbit.it>
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
