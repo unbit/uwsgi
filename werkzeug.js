e9a7bd7da10f1fb049e8f1dacd2668e9c85f73e9
roberto@debian32 <roberto@debian32>
2011-05-23 21:50:04 +0200

    add abstract unix socket

--- werkzeug.js
+++ werkzeug.js
@@ -4,7 +4,7 @@
 		"workers": 8,
 		"module": "werkzeug.testapp:test_app",
 		"master": true,
-		"socket": [ "/tmp/uwsgi.sock", "127.0.0.1:3031" ],
+		"socket": [ "/tmp/uwsgi.sock", "127.0.0.1:3031", "@foobar" ],
 		"pythonpath": [ "/foo", "/bar" ],
 		"show-config": true
 	}
