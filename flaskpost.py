a9ef806a3729d8d9f8f2a5f5523ecbf348fa442c
roberto@debian32 <roberto@debian32>
2011-06-12 09:04:59 +0200

    added flaskpost to address a bug reported on post-buffering

---
+++ flaskpost.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def hello_world():
+    return "<form method='POST' enctype='multipart/form-data' action='/upload'><input type='text' name='pippo'/><input type='file' name='ufile'/><input type='submit' value='upload'/></form>"
+
+@app.route('/upload',  methods=['GET', 'POST'])
+def upload_file():
+    return str(len(request.form.get('pippo', 'boh'))) + request.form.get('pippo', 'boh')
+    return request.files['ufile'].read()
