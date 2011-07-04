c998d6bd1b9c0e1665bc18849672e5653194c183
roberto@longshot <roberto@longshot>
2011-07-04 09:55:13 +0200

    fixed a master bug in socketpair

--- master_utils.c
+++ master_utils.c
@@ -65,7 +65,11 @@ int uwsgi_respawn_worker(int wid) {
 			close(uwsgi.shared->worker_signal_pipe[0]);
 			close(uwsgi.workers[wid].signal_pipe[0]);
 			uwsgi.my_signal_socket = uwsgi.workers[wid].signal_pipe[1];
-			if (uwsgi.shared->spooler_signal_pipe[0] != -1) close (uwsgi.shared->spooler_signal_pipe[0]);
+#ifdef UWSGI_SPOOLER
+        		if (uwsgi.spool_dir && uwsgi.shared->spooler_pid > 0) {
+				if (uwsgi.shared->spooler_signal_pipe[0] != -1) close (uwsgi.shared->spooler_signal_pipe[0]);
+			}
+#endif
 			if ((uwsgi.workers[uwsgi.mywid].respawn_count || uwsgi.cheap)) {
 				for (i = 0; i < 0xFF; i++) {
                 			if (uwsgi.p[i]->master_fixup) {
