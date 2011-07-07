c1775967c885aac45d2fefcacd202bc6b78535c2
roberto@natty32 <roberto@natty32>
2011-07-07 09:25:10 +0200

    fixed build system

--- uwsgiconfig.py
+++ uwsgiconfig.py
@@ -502,7 +502,7 @@ class uConf(object):
                     self.gcc_list.append('json')
                     self.libs.append('-ljansson')
                     has_json = True
-            else:
+            elif self.get('json') == 'true':
                 self.cflags.append("-DUWSGI_JSON")
                 self.gcc_list.append('json')
                 self.libs.append('-ljansson')
@@ -514,7 +514,7 @@ class uConf(object):
                     self.cflags.append("-DUWSGI_LDAP")
                     self.gcc_list.append('ldap')
                     self.libs.append('-lldap')
-            else:
+            elif self.get('ldap') == 'true':
                 self.cflags.append("-DUWSGI_LDAP")
                 self.gcc_list.append('ldap')
                 self.libs.append('-lldap')
@@ -525,7 +525,7 @@ class uConf(object):
                     self.cflags.append("-DUWSGI_ZEROMQ")
                     self.gcc_list.append('proto/zeromq')
                     self.libs.append('-lzmq')
-            else:
+            elif self.get('zeromq') == 'true':
                 self.cflags.append("-DUWSGI_ZEROMQ")
                 self.gcc_list.append('proto/zeromq')
                 self.libs.append('-lzmq')
