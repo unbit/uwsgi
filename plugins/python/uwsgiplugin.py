2c463ad6af6af66e1b364fde26b030634decfbf0
roberto@sirius <roberto@sirius>
2011-08-31 12:02:19 +0200

    a test for libless pyuwsgi

--- plugins/python/uwsgiplugin.py
+++ plugins/python/uwsgiplugin.py
@@ -8,24 +8,27 @@ GCC_LIST = ['python_plugin', 'pyutils', 'pyloader', 'wsgi_handlers', 'wsgi_heade
 CFLAGS = ['-I' + sysconfig.get_python_inc(), '-I' + sysconfig.get_python_inc(plat_specific=True) ] 
 LDFLAGS = []
 
-LIBS = sysconfig.get_config_var('LIBS').split() + sysconfig.get_config_var('SYSLIBS').split()
-if not sysconfig.get_config_var('Py_ENABLE_SHARED'):
+if not 'UWSGI_PYTHON_NOLIB' in os.environ:
+    LIBS = sysconfig.get_config_var('LIBS').split() + sysconfig.get_config_var('SYSLIBS').split()
+    if not sysconfig.get_config_var('Py_ENABLE_SHARED'):
         LIBS.append('-L' + sysconfig.get_config_var('LIBPL'))
-else:
+    else:
 	try:
-		LDFLAGS.append("-L%s" % sysconfig.get_config_var('LIBDIR'))
-		os.environ['LD_RUN_PATH'] = "%s" % (sysconfig.get_config_var('LIBDIR'))
+	    LDFLAGS.append("-L%s" % sysconfig.get_config_var('LIBDIR'))
+	    os.environ['LD_RUN_PATH'] = "%s" % (sysconfig.get_config_var('LIBDIR'))
 	except:
-		LDFLAGS.append("-L%s/lib" % sysconfig.PREFIX)
-		os.environ['LD_RUN_PATH'] = "%s/lib" % sysconfig.PREFIX
+	    LDFLAGS.append("-L%s/lib" % sysconfig.PREFIX)
+	    os.environ['LD_RUN_PATH'] = "%s/lib" % sysconfig.PREFIX
 
 
-version = sysconfig.get_config_var('VERSION')
-try:
-    version = version + sys.abiflags
-except:
-    pass
-LIBS.append('-lpython' + version)
+    version = sysconfig.get_config_var('VERSION')
+    try:
+        version = version + sys.abiflags
+    except:
+        pass
+    LIBS.append('-lpython' + version)
+else:
+    LIBS = []
 
 #if str(PYLIB_PATH) != '':
 #                libs.insert(0,'-L' + PYLIB_PATH)
--- plugins/pyuwsgi/uwsgiplugin.py
+++ plugins/pyuwsgi/uwsgiplugin.py
@@ -1,4 +1,7 @@
 from distutils import sysconfig
+import os
+
+os.environ['UWSGI_PYTHON_NOLIB'] = '1'
 
 NAME='pyuwsgi'
 CFLAGS = ['-I' + sysconfig.get_python_inc(), '-I' + sysconfig.get_python_inc(plat_specific=True)]
