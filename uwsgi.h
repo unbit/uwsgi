cc84a596d62889221204db8102d6d7cffae0eff9
roberto@dexter <roberto@dexter>
2011-09-09 18:44:18 +0200

    better iso99 check

--- uwsgi.h
+++ uwsgi.h
@@ -114,9 +114,10 @@ extern char UWSGI_EMBED_CONFIG_END;
 
 #include <stdarg.h>
 #include <errno.h>
+#ifndef __USE_ISOC99
 #define __USE_ISOC99
+#endif
 #include <ctype.h>
-#undef __USE_ISOC99
 #include <sys/time.h>
 #include <unistd.h>
 
