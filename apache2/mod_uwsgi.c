19686cb19fba4bb4c83ab26a77a58273c80d2d1c
roberto@oneiric64 <roberto@oneiric64>
2011-12-01 14:18:54 +0100

    update the copyright to 2011 in apache module

--- apache2/mod_uwsgi.c
+++ apache2/mod_uwsgi.c
@@ -2,7 +2,7 @@
         
 *** uWSGI/mod_uwsgi ***
 
-Copyright 2009-2010 Unbit S.a.s. <info@unbit.it>
+Copyright 2009-2011 Unbit S.a.s. <info@unbit.it>
      
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
