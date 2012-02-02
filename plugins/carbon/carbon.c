a5aa31f13e2ed689e9b2742a1567e4b7eab7ce07
roberto@debian64 <roberto@debian64>
2012-02-02 11:25:44 +0100

    applied ticket #82

--- plugins/carbon/carbon.c
+++ plugins/carbon/carbon.c
@@ -2,12 +2,6 @@
 
 extern struct uwsgi_server uwsgi;
 
-#define CARBON_OPT_BASE 178000
-#define CARBON_OPT_CARBON		CARBON_OPT_BASE+1
-#define CARBON_OPT_CARBON_ID		CARBON_OPT_BASE+2
-#define CARBON_OPT_CARBON_FREQ		CARBON_OPT_BASE+3
-#define CARBON_OPT_CARBON_TIMEOUT	CARBON_OPT_BASE+4
-
 struct uwsgi_carbon {
 	struct uwsgi_string_list *servers;
 	int freq;
@@ -16,12 +10,10 @@ struct uwsgi_carbon {
 } u_carbon;
 
 struct uwsgi_option carbon_options[] = {
-/*
-	{"carbon", required_argument, 0, CARBON_OPT_CARBON},
-	{"carbon-timeout", required_argument, 0, CARBON_OPT_CARBON_TIMEOUT},
-	{"carbon-freq", required_argument, 0, CARBON_OPT_CARBON_FREQ},
-	{"carbon-id", required_argument, 0, CARBON_OPT_CARBON_ID},
-*/
+	{"carbon", required_argument, 0, "push statistics to the specified carbon server", uwsgi_opt_add_string_list, &u_carbon.servers, UWSGI_OPT_MASTER},
+	{"carbon-timeout", required_argument, 0, "set carbon connection timeout", uwsgi_opt_set_int, &u_carbon.timeout, 0},
+	{"carbon-freq", required_argument, 0, "set carbon push frequency", uwsgi_opt_set_int, &u_carbon.freq, 0},
+	{"carbon-id", required_argument, 0, "set carbon id", uwsgi_opt_set_str, &u_carbon.id, 0},
 	{0, 0, 0, 0, 0, 0, 0},
 
 };
@@ -30,29 +22,6 @@ struct uwsgi_option carbon_options[] = {
 int carbon_init() {
 	return 0;
 }
-
-/*
-int carbon_opt(int i, char *optarg) {
-
-	switch(i) {
-		case CARBON_OPT_CARBON:
-			uwsgi.master_process = 1;
-			uwsgi_string_new_list(&u_carbon.servers, optarg);
-			return 1;
-		case CARBON_OPT_CARBON_ID:
-			u_carbon.id = optarg;
-			return 1;
-		case CARBON_OPT_CARBON_FREQ:
-			u_carbon.freq = atoi(optarg);
-			return 1;
-		case CARBON_OPT_CARBON_TIMEOUT:
-			u_carbon.timeout = atoi(optarg);
-			return 1;
-	}
-
-	return 0;
-}
-*/
 
 void carbon_post_init() {
 
@@ -100,8 +69,10 @@ void carbon_master_cycle() {
 
 			unsigned long long total_rss = 0;
 			unsigned long long total_vsz = 0;
-			unsigned long long avg_rt = 0;
 			unsigned long long total_tx = 0;
+			unsigned long long total_avg_rt = 0; // total avg_rt
+			unsigned long long avg_rt = 0; // per worker avg_rt reported to carbon
+			unsigned long long avg_rt_workers = 0; // number of workers used to calculate total avg_rt
 
 			rlen = snprintf(ptr, 4096, "uwsgi.%s.%s.requests %llu %llu\n", uwsgi.hostname, u_carbon.id, (unsigned long long ) uwsgi.workers[0].requests, (unsigned long long ) uwsgi.current_time);
 			if (rlen < 1) goto clear;
@@ -122,8 +93,17 @@ void carbon_master_cycle() {
 				if (rlen < 1) goto clear;
 				if (write(fd, ptr, rlen) <= 0) { uwsgi_error("write()"); goto clear;}
 
-				avg_rt += uwsgi.workers[i].avg_response_time;
-				rlen = snprintf(ptr, 4096, "uwsgi.%s.%s.worker%d.avg_rt %llu %llu\n", uwsgi.hostname, u_carbon.id, i, (unsigned long long ) uwsgi.workers[i].avg_response_time, (unsigned long long ) uwsgi.current_time);
+				if (uwsgi.workers[i].cheaped) {
+					// also if worker is cheaped than we report its average response time as zero, sending last value might be confusing
+					avg_rt = 0;
+				}
+				else {
+					// global average response time is calcucalted from active/idle workers, cheaped workers are excluded, otherwise it is not accurate
+					avg_rt = uwsgi.workers[i].avg_response_time;
+					avg_rt_workers++;
+					total_avg_rt += uwsgi.workers[i].avg_response_time;
+				}
+				rlen = snprintf(ptr, 4096, "uwsgi.%s.%s.worker%d.avg_rt %llu %llu\n", uwsgi.hostname, u_carbon.id, i, (unsigned long long ) avg_rt, (unsigned long long ) uwsgi.current_time);
 				if (rlen < 1) goto clear;
 				if (write(fd, ptr, rlen) <= 0) { uwsgi_error("write()"); goto clear;}
 
@@ -141,7 +121,7 @@ void carbon_master_cycle() {
 			if (rlen < 1) goto clear;
 			if (write(fd, ptr, rlen) <= 0) { uwsgi_error("write()"); goto clear;}
 
-			rlen = snprintf(ptr, 4096, "uwsgi.%s.%s.avg_rt %llu %llu\n", uwsgi.hostname, u_carbon.id, (unsigned long long ) avg_rt / uwsgi.numproc, (unsigned long long ) uwsgi.current_time);
+			rlen = snprintf(ptr, 4096, "uwsgi.%s.%s.avg_rt %llu %llu\n", uwsgi.hostname, u_carbon.id, (unsigned long long ) total_avg_rt / avg_rt_workers, (unsigned long long ) uwsgi.current_time);
 			if (rlen < 1) goto clear;
 			if (write(fd, ptr, rlen) <= 0) { uwsgi_error("write()"); goto clear;}
 
