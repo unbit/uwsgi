e1f00cbf9641dadd84fc76818c157389a0da3108
roberto@precise64 <roberto@precise64>
2012-02-11 17:20:47 +0100

    better erlang detection

--- plugins/erlang/uwsgiplugin.py
+++ plugins/erlang/uwsgiplugin.py
@@ -1,7 +1,15 @@
+import os
 
 NAME='erlang'
-CFLAGS = []
-LDFLAGS = []
+
+ERLANGPATH = os.environ.get('UWSGICONFIG_ERLANGPATH', 'erl')
+
+includedir = os.popen(ERLANGPATH + " -noshell -noinput -eval \"io:format('~s~n', [code:lib_dir(erl_interface, include)])\" -s erlang halt").read().rstrip()
+libpath = os.popen(ERLANGPATH + " -noshell -noinput -eval \"io:format('~s~n', [code:lib_dir(erl_interface, lib)])\" -s erlang halt").read().rstrip()
+
+CFLAGS = [ '-I' + includedir ]
+LDFLAGS = [ '-L' + libpath ]
+
 LIBS = ['-lei']
 
 GCC_LIST = ['erlang']
