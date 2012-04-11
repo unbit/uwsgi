6b2460c02f2e42538012f036c7ba91f6dc38758d
roberto@precise64 <roberto@precise64>
2012-04-11 07:20:58 +0200

    improved syslog

--- plugins/syslog/syslog_plugin.c
+++ plugins/syslog/syslog_plugin.c
@@ -4,9 +4,59 @@
 
 extern struct uwsgi_server uwsgi;
 
+struct uwsgi_syslog_facility {
+	const char *name;
+	int facility;
+};
+
+struct uwsgi_syslog_facility usf[] = {
+
+	{ "auth",       LOG_AUTH,       },
+        { "authpriv",   LOG_AUTHPRIV,   },
+        { "cron",       LOG_CRON,       },
+        { "daemon",     LOG_DAEMON,     },
+        { "ftp",        LOG_FTP,        },
+#ifdef LOG_INSTALL
+        { "install",    LOG_INSTALL     },
+#endif
+        { "kern",       LOG_KERN,       },
+        { "lpr",        LOG_LPR,        },
+        { "mail",       LOG_MAIL,       },
+#ifdef INTERNAL_MARK
+        { "mark",       INTERNAL_MARK,  },      /* INTERNAL */
+#endif
+#ifdef LOG_NETINFO
+        { "netinfo",    LOG_NETINFO,    },
+#endif
+#ifdef LOG_RAS
+        { "ras",        LOG_RAS         },
+#endif
+#ifdef LOG_REMOTEAUTH
+        { "remoteauth", LOG_REMOTEAUTH  },
+#endif
+        { "news",       LOG_NEWS,       },
+        { "security",   LOG_AUTH        },      /* DEPRECATED */
+        { "syslog",     LOG_SYSLOG,     },
+        { "user",       LOG_USER,       },
+        { "uucp",       LOG_UUCP,       },
+        { "local0",     LOG_LOCAL0,     },
+        { "local1",     LOG_LOCAL1,     },
+        { "local2",     LOG_LOCAL2,     },
+        { "local3",     LOG_LOCAL3,     },
+        { "local4",     LOG_LOCAL4,     },
+        { "local5",     LOG_LOCAL5,     },
+        { "local6",     LOG_LOCAL6,     },
+        { "local7",     LOG_LOCAL7,     },
+#ifdef LOG_LAUNCHD
+        { "launchd",    LOG_LAUNCHD     },
+#endif
+        { NULL,         -1,             }
+};
+
 ssize_t uwsgi_syslog_logger(struct uwsgi_logger *ul, char *message, size_t len) {
 
 	char *syslog_opts;
+	int facility = LOG_DAEMON;
 
 	if (!ul->configured) {
 
@@ -16,10 +66,25 @@ ssize_t uwsgi_syslog_logger(struct uwsgi_logger *ul, char *message, size_t len) 
                 	syslog_opts = "uwsgi";
         	}
 		else {
-			syslog_opts = uwsgi.choosen_logger_arg;
+			char *colon = strchr(uwsgi.choosen_logger_arg, ',');
+			if (colon) {
+				struct uwsgi_syslog_facility *fn = usf;
+				while(fn->name) {
+					if (!strcmp(fn->name, colon+1)) {
+						facility = fn->facility;
+						break;
+					}
+					fn++;
+				}
+				syslog_opts = uwsgi_str(uwsgi.choosen_logger_arg);
+				syslog_opts[colon-uwsgi.choosen_logger_arg] = 0;
+			}
+			else {
+				syslog_opts = uwsgi.choosen_logger_arg;
+			}
 		}
 
-        	openlog(syslog_opts, 0, LOG_DAEMON);
+        	openlog(syslog_opts, 0, facility);
 
 		ul->configured = 1;
 	}
