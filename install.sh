0c6f9faaf1556ee4038cf3e001b956330ae308aa
roberto@precise64 <roberto@precise64>
2012-05-10 08:35:17 +0200

    the installer now wants absolute binary path

--- install.sh
+++ install.sh
@@ -4,6 +4,13 @@ then
   echo "Usage: install.sh <profile> <binary_path>"
   exit 1
 fi
+
+if [ ${2:0:1} != "/" ]
+then
+  echo "uWSGI binary path must be absolute !!!"
+  exit 1
+fi
+
 echo "downloading latest uWSGI tarball..."
 curl -o uwsgi_latest_from_installer.tar.gz http://projects.unbit.it/downloads/uwsgi-latest.tar.gz
 mkdir uwsgi_latest_from_installer
