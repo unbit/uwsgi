4ca33d3f88710c6f52de6d222195575c373564cc
roberto@mrspurr <roberto@mrspurr>
2012-06-10 11:28:32 +0200

    moved lib/rbtree.h to uwsgi.h

--- uwsgi.h
+++ uwsgi.h
@@ -271,13 +271,6 @@ extern int pivot_root(const char *new_root, const char *put_old);
 #include <zmq.h>
 #endif
 
-/* this value are taken from nginx */
-#if defined(__APPLE__) || defined(__freebsd__)
-#define UWSGI_LISTEN_QUEUE -1
-#else
-#define UWSGI_LISTEN_QUEUE 511
-#endif
-
 #define UWSGI_CACHE_MAX_KEY_SIZE 2048
 #define UWSGI_CACHE_FLAG_UNGETTABLE	0x0001
 #define UWSGI_CACHE_FLAG_UPDATE		0x0002
@@ -2374,7 +2367,64 @@ struct uwsgi_subscribe_req {
 };
 
 #ifndef _NO_UWSGI_RB
-#include "lib/rbtree.h"
+
+/*
+
+  *** uWSGI, stripped down version of Linux rbtree ***
+
+
+  Red Black Trees
+  (C) 1999  Andrea Arcangeli <andrea@suse.de>
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+
+*/
+
+struct rb_node
+{
+        unsigned long  rb_parent_color;
+#define RB_RED          0
+#define RB_BLACK        1
+        struct rb_node *rb_right;
+        struct rb_node *rb_left;
+} __attribute__((aligned(sizeof(long))));
+    /* The alignment might seem pointless, but allegedly CRIS needs it */
+
+struct rb_root
+{
+        struct rb_node *rb_node;
+};
+
+#define rb_parent(r)   ((struct rb_node *)((r)->rb_parent_color & ~3))
+#define rb_color(r)   ((r)->rb_parent_color & 1)
+#define rb_is_red(r)   (!rb_color(r))
+#define rb_is_black(r) rb_color(r)
+#define rb_set_red(r)  do { (r)->rb_parent_color &= ~1; } while (0)
+#define rb_set_black(r)  do { (r)->rb_parent_color |= 1; } while (0)
+
+void rb_insert_color(struct rb_node *, struct rb_root *);
+void rb_erase(struct rb_node *, struct rb_root *);
+
+#ifdef __clang__
+void rb_link_node(struct rb_node *, struct rb_node *,
+                                struct rb_node **);
+#else
+inline void rb_link_node(struct rb_node *, struct rb_node *,
+                                struct rb_node **);
+#endif
+
 
 struct uwsgi_rb_timer {
 
