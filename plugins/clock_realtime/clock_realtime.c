c4425627eed177ddc454ae3996158a42e1bb2191
roberto@quantal64 <roberto@quantal64>
2012-08-25 17:23:38 +0200

    added clock realtime plugin

---
+++ plugins/clock_realtime/clock_realtime.c
@@ -0,0 +1,32 @@
+#include "../../uwsgi.h"
+
+// timespecs have nanoseconds resolution
+
+time_t uwsgi_realtime_seconds() {
+	struct timespec ts;
+	clock_gettime(CLOCK_REALTIME, &ts);
+	return ts.tv_sec;
+}
+
+uint64_t uwsgi_realtime_microseconds() {
+        struct timespec ts;
+        clock_gettime(CLOCK_REALTIME, &ts);
+	return (ts.tv_sec * 1000000) + (ts.tv_nsec*1000);
+}
+
+
+static struct uwsgi_clock uwsgi_realtime_clock = {
+	.name = "realtime",
+	.seconds = uwsgi_realtime_seconds,
+	.microseconds = uwsgi_realtime_microseconds,
+};
+
+
+void uwsgi_realtime_clock_load() {
+	uwsgi_register_clock(&uwsgi_realtime_clock);
+}
+
+struct uwsgi_plugin clock_realtime_plugin = {
+	.name = "clock_realtime",
+	.on_load = uwsgi_realtime_clock_load
+};
---
+++ plugins/clock_realtime/uwsgiplugin.py
@@ -0,0 +1,6 @@
+NAME='clock_realtime'
+
+CFLAGS = []
+LDFLAGS = []
+LIBS = []
+GCC_LIST = ['clock_realtime']
