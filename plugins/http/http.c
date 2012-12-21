a67dfdd542f4bcf404656c596e3158823c66099a
Roberto De Ioris <roberto@unbit.it>
2012-12-21 11:32:33 +0100

    fixed a https/corerouter corner-case bug

--- plugins/http/http.c
+++ plugins/http/http.c
@@ -526,10 +526,6 @@ ssize_t hr_read_ssl_body(struct corerouter_session * cs) {
         struct http_session *hs = (struct http_session *) cs;
         int ret = SSL_read(hs->ssl, hs->post_buf->buf, hs->post_buf_max);
         if (ret > 0) {
-                // fix waiting
-                if (cs->event_hook_write) {
-                        uwsgi_cr_hook_write(cs, NULL);
-                }
 		int ret2 = SSL_pending(hs->ssl);
                 if (ret2 > 0) {
 			if (uwsgi_buffer_fix(hs->post_buf, hs->post_buf->len + ret2 )) {
@@ -615,9 +611,6 @@ ssize_t hr_write_ssl_response(struct corerouter_session * cs) {
 
 	if (ret > 0) {
         	cs->buffer_pos += ret;
-		if (cs->event_hook_read) {
-			uwsgi_cr_hook_read(cs, NULL);
-		}
 		// could be a partial write
 		uwsgi_cr_hook_write(cs, hr_write_ssl_response);
         	// ok this response chunk is sent, let's wait for another one
@@ -838,9 +831,6 @@ ssize_t hr_send_expect_continue(struct corerouter_session * cs) {
 		if (ret > 0) {
 			len = ret;
 			cs->buffer_pos += ret;
-			if (cs->event_hook_read) {
-                        	uwsgi_cr_hook_read(cs, NULL);
-                	}
                 	// could be a partial write
                 	uwsgi_cr_hook_write(cs, hr_send_expect_continue);
 			goto done;
@@ -939,11 +929,6 @@ ssize_t hr_recv_http_ssl(struct corerouter_session * cs) {
 	struct http_session *hs = (struct http_session *) cs;
 	int ret = SSL_read(hs->ssl, cs->buffer->buf + cs->buffer_pos, cs->buffer->len - cs->buffer_pos);
 	if (ret > 0) {
-		// fix waiting
-		if (cs->event_hook_write) {
-			uwsgi_cr_hook_write(cs, NULL);
-			uwsgi_cr_hook_read(cs, hr_recv_http_ssl);
-		}
 		int ret2 = SSL_pending(hs->ssl);
 		if (ret2 > 0) {
 			if (uwsgi_buffer_fix(cs->buffer, cs->buffer->len + ret2 )) {
