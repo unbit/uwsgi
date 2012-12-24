2f2876fe6b1b0866bcb1a7eec45fb72a328f54bc
Alex Bubnoff <alex.bubnoff@gmail.com>
2012-12-24 17:36:04 +0700

    fixed build error of pyerl plugin

--- plugins/pyerl/uwsgiplugin.py
+++ plugins/pyerl/uwsgiplugin.py
@@ -1,8 +1,16 @@
+import os
 from distutils import sysconfig
 
 NAME='pyerl'
-CFLAGS = ['-I' + sysconfig.get_python_inc(), '-I' + sysconfig.get_python_inc(plat_specific=True)]
-LDFLAGS = []
-LIBS = []
+
+ERLANGPATH = os.environ.get('UWSGICONFIG_ERLANGPATH', 'erl')
+
+includedir = os.popen(ERLANGPATH + " -noshell -noinput -eval \"io:format('~s~n', [code:lib_dir(erl_interface, include)])\" -s erlang halt").read().rstrip()
+libpath = os.popen(ERLANGPATH + " -noshell -noinput -eval \"io:format('~s~n', [code:lib_dir(erl_interface, lib)])\" -s erlang halt").read().rstrip()
+
+CFLAGS = [ '-I' + includedir, '-I' + sysconfig.get_python_inc(), '-I' + sysconfig.get_python_inc(plat_specific=True)]
+LDFLAGS = [ '-L' + libpath ]
+
+LIBS = ['-lei']
 
 GCC_LIST = ['pyerl']
