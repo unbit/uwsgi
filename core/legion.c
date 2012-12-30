e25e671104216c165d4c69896b3e798a5a2ebe94
Roberto De Ioris <roberto@unbit.it>
2012-12-30 08:34:21 +0100

    removed excessive debugging info in legion

--- core/legion.c
+++ core/legion.c
@@ -830,8 +830,6 @@ void uwsgi_opt_legion(char *opt, char *value, void *foobar) {
                 iv = secret_tmp;
         }
 
-	uwsgi_log("IV = %s\n", iv);
-
 	if (EVP_EncryptInit_ex(ctx, cipher, NULL, (const unsigned char *) secret, (const unsigned char *) iv) <= 0) {
 		uwsgi_error("EVP_EncryptInit_ex()");
 		exit(1);
