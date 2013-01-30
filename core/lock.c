cc93687476f7c5e985698e434053cc78298b7700
Unbit <info@unbit.it>
2013-01-30 18:31:15 +0100

    better support for robust mutexes

--- core/lock.c
+++ core/lock.c
@@ -64,6 +64,12 @@ int uwsgi_pthread_robust_mutexes_enabled = 1;
 #define UWSGI_RWLOCK_SIZE	sizeof(pthread_rwlock_t)
 #endif
 
+#ifndef PTHREAD_PRIO_INHERIT
+int pthread_mutexattr_setprotocol (pthread_mutexattr_t *__attr,
+                                          int __protocol);
+#define PTHREAD_PRIO_INHERIT 1
+#endif
+
 // REMEMBER lock must contains space for both pthread_mutex_t and pthread_mutexattr_t !!! 
 struct uwsgi_lock_item *uwsgi_lock_fast_init(char *id) {
 
@@ -85,11 +91,8 @@ retry:
 	}
 
 #ifdef EOWNERDEAD
-#ifndef PTHREAD_MUTEX_ROBUST_NP
-#define PTHREAD_MUTEX_ROBUST_NP PTHREAD_MUTEX_ROBUST
-#endif
-#ifndef PTHREAD_PRIO_INHERIT
-#define PTHREAD_PRIO_INHERIT 1
+#ifndef PTHREAD_MUTEX_ROBUST
+#define PTHREAD_MUTEX_ROBUST PTHREAD_MUTEX_ROBUST_NP
 #endif
 	if (pthread_mutexattr_setprotocol(&attr, PTHREAD_PRIO_INHERIT)) {
 		uwsgi_log("unable to set PTHREAD_PRIO_INHERIT\n");
