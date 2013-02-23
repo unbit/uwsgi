621a05d87957e91c8bd748b6f1f7429727d06223
Unbit <info@unbit.it>
2013-02-23 13:26:05 +0100

    another heroku python fix

--- plugins/python/uwsgiplugin.py
+++ plugins/python/uwsgiplugin.py
@@ -26,13 +26,21 @@ if not 'UWSGI_PYTHON_NOLIB' in os.environ:
     if not sysconfig.get_config_var('Py_ENABLE_SHARED'):
         libdir = sysconfig.get_config_var('LIBPL')
         # libdir does not exists, try to get it from the venv
+        version = get_python_version()
         if not os.path.exists(libdir):
-            libdir = '%s/lib/python%s/config' % (sys.prefix, get_python_version())
+            libdir = '%s/lib/python%s/config' % (sys.prefix, version)
+        # try skipping abiflag
+        if not os.path.exists(libdir) and version.endswith('m'):
+            version = version[:-1]
+            libdir = '%s/lib/python%s/config' % (sys.prefix, version)
+        # try 3.x style config dir
+        if not os.path.exists(libdir):
+            libdir = '%s/lib/python%s/config-%s' % (sys.prefix, version, get_python_version())
         libpath = '%s/%s' % (libdir, sysconfig.get_config_var('LDLIBRARY'))
         if not os.path.exists(libpath): 
             libpath = '%s/%s' % (libdir, sysconfig.get_config_var('LIBRARY'))
         if not os.path.exists(libpath): 
-            libpath = '%s/libpython%s.a' % (libdir, get_python_version())
+            libpath = '%s/libpython%s.a' % (libdir, version)
         LIBS.append(libpath)
     else:
         try:
