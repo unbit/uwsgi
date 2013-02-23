fbabb14c61a9d2c95a39c8075f3fd6ab2b7f1cc7
Unbit <info@unbit.it>
2013-02-23 13:03:48 +0100

    another heroku check [5]

--- plugins/python/uwsgiplugin.py
+++ plugins/python/uwsgiplugin.py
@@ -27,13 +27,15 @@ if not 'UWSGI_PYTHON_NOLIB' in os.environ:
         libdir = sysconfig.get_config_var('LIBPL')
         # libdir does not exists, try to get it from the venv
         version = get_python_version()
-        print(os.listdir('%s/lib' % sys.prefix))
         if not os.path.exists(libdir):
             libdir = '%s/lib/python%s/config' % (sys.prefix, version)
+        # try skipping abiflag
         if not os.path.exists(libdir) and version.endswith('m'):
             version = version[:-1]
             libdir = '%s/lib/python%s/config' % (sys.prefix, version)
-        print(os.listdir('%s/lib/python%s' % (sys.prefix, version)))
+        # try 3.x style config dir
+        if not os.path.exists(libdir):
+            libdir = '%s/lib/python%s/config-%s' % (sys.prefix, version, get_python_version())
         libpath = '%s/%s' % (libdir, sysconfig.get_config_var('LDLIBRARY'))
         if not os.path.exists(libpath): 
             libpath = '%s/%s' % (libdir, sysconfig.get_config_var('LIBRARY'))
