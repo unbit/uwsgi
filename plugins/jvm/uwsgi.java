54b4189ab997a7c43bb0227ea3856b721c5e2366
Unbit <info@unbit.it>
2013-03-03 18:43:22 +0100

    added uwsgi.java as the base for JVM request plugins

---
+++ plugins/jvm/uwsgi.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class uwsgi {
+
+	public class request_body extends InputStream {
+		public native int read();
+		public native int read(byte[] b);
+		public native int available();
+	}
+
+	public interface SignalHandler {
+		void function(int signum);
+	}
+
+	public interface RpcFunction {
+		String function(String... args);
+	}
+
+	public static native int worker_id();
+	public static native void register_signal(int signum, String target, SignalHandler sh);
+	public static native void register_rpc(String name, RpcFunction rf);
+
+}
