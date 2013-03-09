26c6ec4283184ab583715fe00d7173b26151d42a
Unbit <info@unbit.it>
2013-03-09 11:03:46 +0100

    fixed admin and echo plugins

--- plugins/echo/echo_plugin.c
+++ plugins/echo/echo_plugin.c
@@ -4,7 +4,7 @@ extern struct uwsgi_server uwsgi;
 
 static int uwsgi_echo_request(struct wsgi_request *wsgi_req) {
 
-	return uwsgi_response_write_body_do(wsgi_req, wsgi_req->buffer, wsgi_req->uh.pktsize);
+	return uwsgi_response_write_body_do(wsgi_req, wsgi_req->buffer, wsgi_req->uh->pktsize);
 }
 
 struct uwsgi_plugin echo_plugin = {
