d8a61a920ed03677cc96bad31efebe9b8967ba99
Unbit <info@unbit.it>
2013-04-03 12:41:07 +0200

    allows building a monolithic gridfs binary

--- plugins/gridfs/uwsgiplugin.py
+++ plugins/gridfs/uwsgiplugin.py
@@ -8,7 +8,9 @@ LDFLAGS = []
 LIBS = []
 if not 'UWSGI_MONGODB_NOLIB' in os.environ:
     LIBS.append('-lmongoclient')
+    LIBS.append('-lstdc++')
     LIBS.append('-lboost_thread')
+    LIBS.append('-lboost_system')
     LIBS.append('-lboost_filesystem')
 
 GCC_LIST = ['plugin', 'gridfs.cc']
