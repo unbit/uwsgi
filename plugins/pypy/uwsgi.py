378f78bd873c25f4840935489710bc6ad0923d92
Unbit <info@unbit.it>
2013-05-16 18:03:35 +0200

    removed uwsgi.py

--- plugins/pypy/uwsgi.py
+++
@@ -1,9 +0,0 @@
-
-class UWSGIState(object):
-    def __init__(self):
-        self.callables = {} # a mapping from app-id to application
-
-class WSGIRequest(object):
-    pass
-
-uwsgi_global_state = UWSGIState() # :-((
