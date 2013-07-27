ac17b0910eb4259e566b92280e854bd539ebda31
Unbit <info@unbit.it>
2013-07-27 22:09:29 +0200

    another attempt to make heroku happy [2]

--- plugins/rack/rack_plugin.c
+++ plugins/rack/rack_plugin.c
@@ -457,7 +457,7 @@ int uwsgi_rack_init(){
 
 #ifdef RUBY19
 	int argc = 2;
-	char *sargv[] = { (char *) "uwsgi", (char *) "-e0", NULL };
+	char *sargv[] = { (char *) uwsgi_str("uwsgi"), (char *) uwsgi_str("-e0"), NULL };
 	char **argv = sargv;
 #endif
 
@@ -469,7 +469,7 @@ int uwsgi_rack_init(){
 	ruby_sysinit(&argc, &argv);
 	RUBY_INIT_STACK
 	ruby_init();
-	//ruby_process_options(argc, argv);
+	ruby_process_options(argc, argv);
 #else
 	ruby_init();
 	ruby_init_loadpath();
