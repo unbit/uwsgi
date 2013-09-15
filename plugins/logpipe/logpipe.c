c7a22c3b539070edb21930a3287df814eec40de7
Unbit <info@unbit.it>
2013-09-15 12:02:14 +0200

    improved logpipe

--- plugins/logpipe/logpipe.c
+++ plugins/logpipe/logpipe.c
@@ -1,49 +1,61 @@
-#include "../../uwsgi.h"
+#include <uwsgi.h>
 
-ssize_t uwsgi_pipe_logger(struct uwsgi_logger *ul, char *message, size_t len) {
-    if (!ul->configured) {
-        ul->configured = 1;
-        if (ul->arg) {
-            int pipefd[2];
-            if (-1 == pipe(pipefd)) {
-                perror("pipe");
-                uwsgi_error("Can't create pipe");
-                return 0;
-            }
-            if (fork()) {
-                close(pipefd[0]);
-                ul->fd = pipefd[1];
-            } else {
-                // child
-                setsid();
-                close(pipefd[1]);
-                dup2(pipefd[0], STDIN_FILENO);
-                close(pipefd[0]);
-                uwsgi_exec_command_with_args(ul->arg);
-                return 0; // don't come here.
-            }
-        }
-    }
+/*
 
-    if (ul->fd) {
-        int err;
-        err = write(ul->fd, message, len);
-        if (err < 0) {
-            perror("write");
-            uwsgi_error("Can't write to pipe.");
-            close(ul->fd);
-            ul->fd = 0;
-        }
-        return err;
-    }
-    return 0;
+	The pipe logger
+
+	Author: INADA Naoki
+
+	every log line is sent to the stdin of an external process	
+
+	Example:
+
+		req-logger = pipe:/usr/local/bin/mylogger
+
+*/
+
+static ssize_t uwsgi_pipe_logger(struct uwsgi_logger *ul, char *message, size_t len) {
+	if (!ul->configured) {
+		if (ul->arg) {
+			int pipefd[2];
+			if (-1 == pipe(pipefd)) {
+				uwsgi_error("uwsgi_pipe_logger()/pipe()");
+				return -1;
+			}
+			if (fork()) {
+				close(pipefd[0]);
+				ul->fd = pipefd[1];
+			}
+			else {
+				// child
+				setsid();
+				close(pipefd[1]);
+				dup2(pipefd[0], STDIN_FILENO);
+				close(pipefd[0]);
+				uwsgi_exec_command_with_args(ul->arg);
+				exit(1);	// if here something seriously failed
+			}
+		}
+
+		ul->configured = 1;
+	}
+
+	int err = write(ul->fd, message, len);
+	// on failed writes, re-configure the logger
+	if (err <= 0) {
+		uwsgi_error("uwsgi_pipe_logger()/write()");
+		close(ul->fd);
+		ul->configured = 0;
+		return err;
+	}
+	return 0;
 }
 
-void uwsgi_pipe_logger_register() {
-    uwsgi_register_logger("pipe", uwsgi_pipe_logger);
+static void uwsgi_pipe_logger_register() {
+	uwsgi_register_logger("pipe", uwsgi_pipe_logger);
 }
 
 struct uwsgi_plugin logpipe_plugin = {
-    .name = "logpipe",
-    .on_load = uwsgi_pipe_logger_register,
+	.name = "logpipe",
+	.on_load = uwsgi_pipe_logger_register,
 };
