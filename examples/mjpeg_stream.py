92b6c62cac80ae1f812270736e5b579f392f8f7b
Unbit <info@unbit.it>
2013-11-30 15:43:17 +0100

    improved mjpeg example

--- examples/mjpeg_stream.py
+++ examples/mjpeg_stream.py
@@ -10,16 +10,16 @@ def application(env, start_response):
 				('Cache-Control', 'no-cache'),
 				('Cache-Control', 'private'),
 				('Pragma', 'no-cache'),
-				('Content-Type', 'multipart/x-mixed-replace; boundary=' + boundary),
+				('Content-Type', 'multipart/x-mixed-replace; boundary=--' + boundary),
 				]
 			)
 
-	yield "--%s\n" % boundary
+	yield "--%s\r\n" % boundary
 
 	while 1:
 		yield "Content-Type: image/jpeg\r\n\r\n"
 		print os.system('screencapture -t jpg -m -T 1 screenshot.jpg')
 		f = open('screenshot.jpg')
                 yield env['wsgi.file_wrapper'](f)
-		yield "--%s\n" % boundary
+		yield "--%s\r\n" % boundary
 		#os.system('./isightcapture -w 640 -h 480 screenshot.jpg')
