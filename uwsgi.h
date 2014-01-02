d9aeb26b725bed668653ffa92ba27c2c58ceeb24
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2014-01-02 19:43:19 +0100

    uwsgi: save some memory reordering wsgi_request struct
    Continuing the saving memory new year resolution this time is
    wsgi_request time.
    
    With the help of pahole (from dwarves) by shuffling some fields
    we get from:
    
        /* size: 872, cachelines: 14, members: 183 */
        /* sum members: 803, holes: 33, sum holes: 69 */
    
    to:
    
        /* size: 808, cachelines: 13, members: 183 */
        /* sum members: 803, holes: 2, sum holes: 5 *
    
    Which are 64 bytes saved. struct uwsgi_core saves 64
    bytes for free too.
    
    Please note these measurements are done on a 32bit machine, but
    since the savings are found by moving smaller that 32 bit fields
    it should be the similar on 64bit.

--- uwsgi.h
+++ uwsgi.h
@@ -1322,77 +1322,77 @@ struct wsgi_request {
 
 	char *uri;
 	uint16_t uri_len;
-	char *remote_addr;
 	uint16_t remote_addr_len;
+	char *remote_addr;
 	char *remote_user;
 	uint16_t remote_user_len;
-	char *query_string;
 	uint16_t query_string_len;
+	char *query_string;
 	char *protocol;
 	uint16_t protocol_len;
-	char *method;
 	uint16_t method_len;
+	char *method;
 	char *scheme;
 	uint16_t scheme_len;
-	char *https;
 	uint16_t https_len;
+	char *https;
+	int script_name_pos;
 	char *script_name;
 	uint16_t script_name_len;
-	int script_name_pos;
 
-	char *host;
 	uint16_t host_len;
+	char *host;
 
 	char *content_type;
 	uint16_t content_type_len;
 
-	char *document_root;
 	uint16_t document_root_len;
+	char *document_root;
 
 	char *user_agent;
 	uint16_t user_agent_len;
 
-	char *encoding;
 	uint16_t encoding_len;
+	char *encoding;
 
 	char *referer;
 	uint16_t referer_len;
 
-	char *cookie;
 	uint16_t cookie_len;
+	char *cookie;
 
+	int path_info_pos;
 	char *path_info;
 	uint16_t path_info_len;
-	int path_info_pos;
 
-	char *authorization;
 	uint16_t authorization_len;
+	char *authorization;
 
 	uint16_t via;
 
-	char *script;
 	uint16_t script_len;
+	char *script;
 	char *module;
 	uint16_t module_len;
-	char *callable;
 	uint16_t callable_len;
+	char *callable;
 	char *home;
 	uint16_t home_len;
 
-	char *file;
 	uint16_t file_len;
+	char *file;
 
 	char *paste;
 	uint16_t paste_len;
 
-	char *chdir;
 	uint16_t chdir_len;
+	char *chdir;
 
 	char *touch_reload;
 	uint16_t touch_reload_len;
 
-	char *cache_get;
 	uint16_t cache_get_len;
+	char *cache_get;
 
 	char *if_modified_since;
 	uint16_t if_modified_since_len;
@@ -1506,17 +1506,16 @@ struct wsgi_request {
 	// websocket specific headers
 	char *http_sec_websocket_key;
 	uint16_t http_sec_websocket_key_len;
-	char *http_origin;
 	uint16_t http_origin_len;
+	char *http_origin;
 	char *http_sec_websocket_protocol;
 	uint16_t http_sec_websocket_protocol_len;
-	
 
-	struct uwsgi_buffer *chunked_input_buf;
 	uint8_t chunked_input_parser_status;
+	uint8_t chunked_input_complete;
+	struct uwsgi_buffer *chunked_input_buf;
 	ssize_t chunked_input_chunk_len;
 	size_t chunked_input_need;
-	uint8_t chunked_input_complete;
         size_t chunked_input_decapitate;
 
 	uint64_t stream_id;
