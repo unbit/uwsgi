daeaf1b23100d194606982ac6f41a22fc18075b2
Babacar TALL <babacar@1000mercis.com>
2014-05-20 16:46:25 +0200

    Doesn't use uwsgi_metric_get for the comparison on uwsgi_metric_set_max and uwsgi_metric_set_min (it introduce a deadlock)

--- core/metrics.c
+++ core/metrics.c
@@ -704,7 +704,7 @@ int64_t uwsgi_metric_getn(char *name, size_t nlen, char *oid, size_t olen) {
 
 int uwsgi_metric_set_max(char *name, char *oid, int64_t value) {
 	um_op;
-    if (value > uwsgi_metric_get(name, oid))
+    if (value > *um->value)
         *um->value = value;
 	uwsgi_rwunlock(uwsgi.metrics_lock);
 	return 0;
@@ -712,7 +712,7 @@ int uwsgi_metric_set_max(char *name, char *oid, int64_t value) {
 
 int uwsgi_metric_set_min(char *name, char *oid, int64_t value) {
 	um_op;
-    if ((value > um->initial_value || 0) && value < uwsgi_metric_get(name, oid))
+    if ((value > um->initial_value || 0) && value < *um->value)
         *um->value = value;
 	uwsgi_rwunlock(uwsgi.metrics_lock);
 	return 0;
