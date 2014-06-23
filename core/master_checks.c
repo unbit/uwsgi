17ec600146809804642e6a5e7f8d8a482d6b6f78
Unbit <info@unbit.it>
2014-06-23 12:39:58 +0200

    implemented total harakiri

--- core/master_checks.c
+++ core/master_checks.c
@@ -160,6 +160,12 @@ int uwsgi_master_check_workers_deadline() {
 	int i,j;
 	int ret = 0;
 	for (i = 1; i <= uwsgi.numproc; i++) {
+		if (uwsgi.workers[i].harakiri_total > 0) {
+                	if (uwsgi.workers[i].harakiri_total < (time_t) uwsgi.current_time) {
+                        	trigger_harakiri(i);
+                                ret = 1;
+			}
+		}
 		for(j=0;j<uwsgi.cores;j++) {
 			/* first check for harakiri */
 			if (uwsgi.workers[i].cores[j].harakiri > 0) {
