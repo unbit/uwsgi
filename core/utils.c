fce15b7ab58e42622262465abfacac1b301e049e
Unbit <info@unbit.it>
2014-08-26 07:25:25 +0200

    fixed #693

--- core/utils.c
+++ core/utils.c
@@ -1035,7 +1035,11 @@ void uwsgi_destroy_request(struct wsgi_request *wsgi_req) {
 	// reset for avoiding following requests to fail on non-uwsgi protocols
 	// thanks Marko Tiikkaja for catching it
 	wsgi_req->uh->pktsize = 0;
+
+	// some plugins expected async_id to be defined before setup
+        int tmp_id = wsgi_req->async_id;
         memset(wsgi_req, 0, sizeof(struct wsgi_request));
+        wsgi_req->async_id = tmp_id;
 }
 
 // finalize/close/free a request
@@ -1181,9 +1185,10 @@ void uwsgi_close_request(struct wsgi_request *wsgi_req) {
 	wsgi_req->uh->pktsize = 0;
 	tmp_id = wsgi_req->async_id;
 	memset(wsgi_req, 0, sizeof(struct wsgi_request));
-
+	// some plugins expected async_id to be defined before setup
+	wsgi_req->async_id = tmp_id;
 	// yes, this is pretty useless but we cannot ensure all of the plugin have the same behaviour
-	uwsgi.workers[uwsgi.mywid].cores[tmp_id].in_request = 0;
+	uwsgi.workers[uwsgi.mywid].cores[wsgi_req->async_id].in_request = 0;
 
 	if (uwsgi.max_requests > 0 && uwsgi.workers[uwsgi.mywid].delta_requests >= (uwsgi.max_requests + ((uwsgi.mywid-1) * uwsgi.max_requests_delta))
 	    && (end_of_request - (uwsgi.workers[uwsgi.mywid].last_spawn * 1000000) >= uwsgi.min_worker_lifetime * 1000000)) {
