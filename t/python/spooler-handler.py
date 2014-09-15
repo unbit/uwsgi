650e939e3b8eb44962d826e2bc47ae7af32eea73
Aldur <adrianodl@hotmail.it>
2014-09-15 09:55:56 +0200

    add test regarding spooler and related task priority

--- t/python/spooler-handler.py
+++ t/python/spooler-handler.py
@@ -2,15 +2,32 @@
 # coding = utf-8
 
 from __future__ import print_function
+from constants import tasks, LOGFILE
+from os import remove
 import uwsgi
+
+counter = 0
 
 
 def spoolerHandler(env):
+	global counter
 	# Spooler is handling a task
-	uwsgi.signal(17)
-	print("%s" % env['name'])
+	with open(LOGFILE, "a") as log:
+		print("%s" % (env['name']), file=log)
+
+	counter += 1
+
+	if counter == len(tasks):
+		# Each task has been processed.
+		uwsgi.signal(17)
 
 	# Spooler has done handling the task
 	return uwsgi.SPOOL_OK
 
 uwsgi.spooler = spoolerHandler
+
+# Clear the logfile
+try:
+	remove(LOGFILE)
+except OSError, e:  # log does not exist
+	print(e)
