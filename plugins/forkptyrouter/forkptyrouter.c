0eb211362f7634f514d4e9f0a9e21ad50608cf21
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2014-10-27 11:51:07 +0100

    Fixup forkpty ifdefery for dragonfly / freebsd
    They are negated so we should AND them

--- plugins/forkptyrouter/forkptyrouter.c
+++ plugins/forkptyrouter/forkptyrouter.c
@@ -23,7 +23,7 @@ extern struct uwsgi_server uwsgi;
 #elif defined(__FreeBSD__) || defined(__DragonFly__)
 #include <libutil.h>
 #endif
-#if !defined(__FreeBSD__) || !defined(__DragonFly__)
+#if !defined(__FreeBSD__) && !defined(__DragonFly__)
 #include <utmp.h>
 #endif
 
