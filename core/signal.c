83acb83075ef8236018123aa48c48c1b610d9364
Samuel Čavoj <sammko@sammserver.com>
2015-03-16 20:54:53 +0100

    Fix grammar.
    Grammar is important!

--- core/signal.c
+++ core/signal.c
@@ -493,7 +493,7 @@ void uwsgi_receive_signal(struct wsgi_request *wsgi_req, int fd, char *name, int
 
 destroy:
 	// better to kill the whole worker...
-	uwsgi_log_verbose("uWSGI %s %d screams: UAAAAAAH my master disconnected: i will kill myself !!!\n", name, id);
+	uwsgi_log_verbose("uWSGI %s %d screams: UAAAAAAH my master disconnected: I will kill myself!!!\n", name, id);
 	end_me(0);
 
 }
