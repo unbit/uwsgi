0e339cf968ff61d460150b2232a272cbbf3d8ac2
Unbit <info@unbit.it>
2015-05-13 15:42:41 +0200

    added bigranges sharedarea tests

---
+++ t/sharedarea/bigranges.py
@@ -0,0 +1,16 @@
+import uwsgi
+import unittest
+
+class SharedareaTest(unittest.TestCase):
+
+    def test_32(self):
+        pos = 2L * (1024L ** 3)
+        uwsgi.sharedarea_write32(0, pos, 17)
+        self.assertEqual(uwsgi.sharedarea_read32(0, pos), 17)
+
+    def test_64(self):
+        pos = 2L * (1024L ** 3)
+        uwsgi.sharedarea_write64(0, pos, 30)
+        self.assertEqual(uwsgi.sharedarea_read64(0, pos), 30)
+
+unittest.main()
