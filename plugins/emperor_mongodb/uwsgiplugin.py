c0634013c437e5b289d2b5ae9ecce40aedf59228
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2015-05-21 10:18:56 +0400

    Added -lboost_system to avoid import errors

--- plugins/emperor_mongodb/uwsgiplugin.py
+++ plugins/emperor_mongodb/uwsgiplugin.py
@@ -13,7 +13,8 @@ if 'UWSGI_MONGODB_NOLIB' not in os.environ:
     LIBS.append('-lmongoclient')
     LIBS.append('-lboost_thread')
     LIBS.append('-lboost_filesystem')
+    LIBS.append('-lboost_system')
     LIBS.append('-lboost_regex')
-    
+
 
 GCC_LIST = ['plugin', 'emperor_mongodb.cc']
