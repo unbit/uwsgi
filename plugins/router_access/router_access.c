unbit/uwsgi

commit 837da66440576a3e6ed8889799a34b6387e83e9b

plugins/router_access: fix copy-paste error
Reported by Coverity as CID #1308276

plugins/router_access/router_access.c
@@ -37,7 +37,7 @@ static int uwsgi_routing_func_access(struct wsgi_request *wsgi_req, struct uwsgi
 			goto forbidden;		
 		}
 		if (!urac->action) urac->action = "hosts";
-		if (!urac->daemon) urac->action = "uwsgi";
+		if (!urac->daemon) urac->deamon = "uwsgi";
 		ur->data2 = urac;
 	}
 

commit da897a36fcbc81742ddd26cca2a88332f24459ce

Fix compilation in router_access
Typo introduced in 837da66440576a3e6ed8889799a34b6387e83e9b

plugins/router_access/router_access.c
@@ -37,7 +37,7 @@ static int uwsgi_routing_func_access(struct wsgi_request *wsgi_req, struct uwsgi
 			goto forbidden;		
 		}
 		if (!urac->action) urac->action = "hosts";
-		if (!urac->daemon) urac->deamon = "uwsgi";
+		if (!urac->daemon) urac->daemon = "uwsgi";
 		ur->data2 = urac;
 	}
 