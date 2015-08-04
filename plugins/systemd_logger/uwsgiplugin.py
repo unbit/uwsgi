c272c94d22a70e67e58b1cbe5abf84a916604847
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2015-08-04 11:13:32 +0200

    systemd_logger: fix build with recent systemd
    Since systemd 209 all the functionalities of libsystemd-* have
    been merged in libsystemd.

--- plugins/systemd_logger/uwsgiplugin.py
+++ plugins/systemd_logger/uwsgiplugin.py
@@ -1,7 +1,10 @@
 import os
-NAME='systemd_logger'
+
+NAME = 'systemd_logger'
 
 CFLAGS = os.popen('pkg-config --cflags libsystemd-journal').read().rstrip().split()
+CFLAGS += os.popen('pkg-config --cflags libsystemd').read().rstrip().split()
 LDFLAGS = []
-LIBS = os.popen('pkg-config --libs libsystemd-journal').read().rstrip().split() 
+LIBS = os.popen('pkg-config --libs libsystemd-journal').read().rstrip().split()
+LIBS += os.popen('pkg-config --libs libsystemd').read().rstrip().split()
 GCC_LIST = ['systemd_logger']
