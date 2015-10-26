32cb88f88e178b688370b2dd23ec134b9733373b
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2015-10-26 18:12:25 +0100

    uwsgi_tmpfd: set umask before calling mkstemp
    Reported by Coverity as CID #1100820

--- plugins/tuntap/tuntap.c
+++ plugins/tuntap/tuntap.c
@@ -189,9 +189,7 @@ static void uwsgi_tuntap_client() {
 			pthread_create(&t, NULL, uwsgi_tuntap_loop, uttr);
 		}
 		else {
-			if (uwsgi_tuntap_device(usl->value) < 0) {
-				// foo
-			}
+			uwsgi_tuntap_device(usl->value);
 		}
 	}
 }
