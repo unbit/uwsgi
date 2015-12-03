a67bed6d590c5fb47dc22388b4f53c8860ad9717
albo <alexandre.bonnetain@1000mercis.com>
2015-12-03 12:45:18 +0100

    remove consumer from spooler_get_task example code

--- t/spooler/read.py
+++ t/spooler/read.py
@@ -1,4 +1,4 @@
-# uswgi
+# uswgi --spooler-external ../other_app/myspool --mule --mule
 
 from uwsgidecorators import spoolraw, muleloop
 
@@ -8,15 +8,17 @@ import collections
 import random
 import os
 
+spooling_directory = "../other_app/myspool"
+
 @muleloop(1)
 def reader():
     c = collections.Counter()
 
-    for file in os.listdir("myspool"):
+    for file in os.listdir(spooling_directory):
         try:
-            c[uwsgi.spooler_get_task("myspool/" + file)["dest"]] += 1
-        except Exception:
-            pass
+            c[uwsgi.spooler_get_task(spooling_directory + "/" + file)["dest"]] += 1
+        except Exception as e:
+            print(e)
 
     print(c)
     time.sleep(5)
@@ -27,9 +29,3 @@ projects = ["uwsgi", "python", "ruby", "nginx", "memcache"]
 def producer():
     uwsgi.spool(ud_spool_func="consumer", dest=random.choice(projects))
     time.sleep(2)
-
-@spoolraw
-def consumer(args):
-    print("project : " + args["dest"])
-    time.sleep(3)
-    return uwsgi.SPOOL_OK
