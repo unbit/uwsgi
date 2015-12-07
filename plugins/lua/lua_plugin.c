e9c4bfe71ca6627f5dbb30037b5e3362e730319b
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2015-12-07 21:26:45 +0100

    plugins/lua: fix off by one in strncpy
    len is not taking into account null termination.
    Fix regression from cbca6b93b7f5c277bec2172b092ee131fe3c8fa6

--- plugins/lua/lua_plugin.c
+++ plugins/lua/lua_plugin.c
@@ -983,7 +983,7 @@ static int uwsgi_api_register_signal(lua_State *L) {
 		uwsgi_lock(uwsgi.signal_table_lock);
 
 		// master-proc checks receiver
-		strncpy(use->receiver, who, len);
+		strcpy(use->receiver, who, len+1);
 
 		// if master has not any handler
 		// if has, then only update receiver
