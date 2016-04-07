7955ee29b934dc406e1423ab053caa35aa37ca79
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2016-04-07 19:10:55 +0300

    Revert "use _full write methods in rados plugin to truncate PUT request objects"
    rados_write_full always overwrites object.
    So if object size is greater than buffer_size, then only last part of an
    object will be stored.
    
    This reverts commit 3cd3559399fac5ecc701396b1d503224f19062c0.

--- plugins/rados/rados.c
+++ plugins/rados/rados.c
@@ -160,7 +160,7 @@ static int uwsgi_rados_put(struct wsgi_request *wsgi_req, rados_ioctx_t ctx, cha
                 char *body =  uwsgi_request_body_read(wsgi_req, UMIN(remains, buffer_size) , &body_len);
                 if (!body || body == uwsgi.empty) goto error;
 		if (uwsgi.async < 1) {
-			if (rados_write_full(ctx, key, body, body_len) < 0) {
+			if (rados_write(ctx, key, body, body_len, off) < 0) {
 				return -1;
 			}
 		}
@@ -182,7 +182,7 @@ static int uwsgi_rados_put(struct wsgi_request *wsgi_req, rados_ioctx_t ctx, cha
                 		free(urcb);
                 		goto error;
         		}
-        		if (rados_aio_write_full(ctx, key, comp, body, body_len) < 0) {
+        		if (rados_aio_write(ctx, key, comp, body, body_len, off) < 0) {
                 		free(urcb);
                 		rados_aio_release(comp);
                 		goto error;
