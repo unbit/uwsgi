0684b171422674951e2f3a56747049229aa83245
Mathieu Arnold <mat@FreeBSD.org>
2016-05-09 09:50:26 +0200

    apache mod proxy: Fix failure to compile against apache 2.4.20
    Fix #1244

--- apache2/mod_proxy_uwsgi.c
+++ apache2/mod_proxy_uwsgi.c
@@ -257,7 +257,7 @@ static request_rec *ap_proxy_make_fake_req(conn_rec *c, request_rec *r)
     return rp;
 }
 
-static apr_status_t ap_proxy_buckets_lifetime_transform(request_rec *r,
+apr_status_t ap_proxy_buckets_lifetime_transform(request_rec *r,
         apr_bucket_brigade *from, apr_bucket_brigade *to)
 {
     apr_bucket *e;
