0e07a7032a98952f21a83140dd6f1eee61ce436b
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2016-08-17 10:22:47 +0200

    plugins/pyuwsgi: don't compile it on python3
    As it won't compile

--- plugins/pyuwsgi/uwsgiplugin.py
+++ plugins/pyuwsgi/uwsgiplugin.py
@@ -1,5 +1,5 @@
 from distutils import sysconfig
-import os
+import os, sys
 
 os.environ['UWSGI_PYTHON_NOLIB'] = '1'
 
@@ -8,4 +8,8 @@ CFLAGS = ['-I' + sysconfig.get_python_inc(), '-I' + sysconfig.get_python_inc(pla
 LDFLAGS = []
 LIBS = []
 
-GCC_LIST = ['pyuwsgi']
+PY3 = sys.version_info[0] >= 3
+if not PY3:
+    GCC_LIST = ['pyuwsgi']
+else:
+    GCC_LIST = []
