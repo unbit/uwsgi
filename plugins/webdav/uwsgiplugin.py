efe24d9d830db797fbd5f109231b2da38bbedb65
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2016-09-13 11:51:26 +0200

    plugins/webdav: fix compilation with glibc 2.24
    This version of the GNU C library deprecates readdir_r and suggests
    to use readdir instead, as it is thread safe too.
    
    In the meantime we sort out if the other platforms readdir
    implementations are thread safe too we silence deprecations warnings
    for this plugin.
    
    Cherry picked from 23c59ccff8ccc030c3f26e87dd65e1d94259a0db

--- plugins/webdav/uwsgiplugin.py
+++ plugins/webdav/uwsgiplugin.py
@@ -1,7 +1,9 @@
 import os
 
-NAME='webdav'
-CFLAGS = os.popen('xml2-config --cflags').read().rstrip().split()
+NAME = 'webdav'
+
+CFLAGS = ['-Wno-deprecated-declarations']
+CFLAGS += os.popen('xml2-config --cflags').read().rstrip().split()
 LDFLAGS = []
 LIBS = os.popen('xml2-config --libs').read().rstrip().split()
 
