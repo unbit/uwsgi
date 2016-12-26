c975cb63c56c7ebc5b54c11ebf3a19ce8dd28f28
Jonas Smedegaard <dr@jones.dk>
2016-12-26 16:39:59 +0100

    plugins/emperor_pg: avoid informational-only Postgres flags
    pg_config options --cflags and --ldflags are both documented as being
    informational about what was used for building PostgreSQL itself (not
    intended for linking against its libraries).
    
    Reference: http://bugs.debian.org/847207

--- plugins/emperor_pg/uwsgiplugin.py
+++ plugins/emperor_pg/uwsgiplugin.py
@@ -1,9 +1,12 @@
 import os
 
-NAME='emperor_pg'
-CFLAGS = os.popen('pg_config --cflags').read().rstrip().split()
-CFLAGS.append('-I' + os.popen('pg_config --includedir').read().rstrip())
-LDFLAGS = os.popen('pg_config --ldflags').read().rstrip().split()
-LIBS = ['-L' + os.popen('pg_config --libdir').read().rstrip(), '-lpq']
+NAME = 'emperor_pg'
+
+CFLAGS = ['-I' + os.popen('pg_config --includedir').read().rstrip()]
+LDFLAGS = []
+LIBS = [
+    '-L' + os.popen('pg_config --libdir').read().rstrip(),
+    '-lpq'
+]
 
 GCC_LIST = ['emperor_pg']
