f99a969de6392cb4dcba4d950dea52b0da96f12f
Jon Dufresne <jon.dufresne@gmail.com>
2017-01-26 18:26:47 -0800

    Add the "License" classifier to setup.py
    Also updated the "license" line to be explicit about any later version.

--- setup.py
+++ setup.py
@@ -130,11 +130,12 @@ setup(
     description='The uWSGI server',
     author='Unbit',
     author_email='info@unbit.it',
-    license='GPL2',
+    license='GPLv2+',
     py_modules=['uwsgidecorators'],
     distclass=uWSGIDistribution,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
