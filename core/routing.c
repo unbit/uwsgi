c73257aacc6f1a21b59273a95e3e20e249ac9f6e
Curtis Maloney <curtis@tinbrain.net>
2017-09-02 17:27:41 +1000

    Replace incorrect breaks with fallthrough comment
    Patch 0a14d0f0425 replaced the implicit fall throughs with explicit breaks, which is clearly wrong.
    Instead, we put comments that GCC will recognise, as documented:
    
    https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html

--- core/routing.c
+++ core/routing.c
@@ -1792,10 +1792,10 @@ static int uwsgi_route_condition_ipv6in(struct wsgi_request *wsgi_req, struct uw
 
 	int i = (pfxlen / 32);
 	switch (i) {
-	case 0: mask[0] = 0; break;
-	case 1: mask[1] = 0; break;
-	case 2: mask[2] = 0; break;
-	case 3: mask[3] = 0; break;
+	case 0: mask[0] = 0; /* fallthrough */
+	case 1: mask[1] = 0; /* fallthrough */
+	case 2: mask[2] = 0; /* fallthrough */
+	case 3: mask[3] = 0; /* fallthrough */
 	}
 
 	if (pfxlen % 32)
