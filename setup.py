e779b8f982d6b5543a106c5453be7193faaa48c6
Jon Dufresne <jon.dufresne@gmail.com>
2018-01-02 19:54:03 -0800

    Add trove classifiers for all supported Python versions
    Help users know, at a glance, if uWSGI is compatible with their project.

--- setup.py
+++ setup.py
@@ -134,8 +134,14 @@ setup(
     py_modules=['uwsgidecorators'],
     distclass=uWSGIDistribution,
     classifiers=[
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     install_requires=get_extra_require()
 )
