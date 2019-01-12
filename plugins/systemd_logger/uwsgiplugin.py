7b6251080499be74657f8af9c66fdb68a55d5465
Chris Mayo <aklhfex@gmail.com>
2019-01-12 16:48:38 +0000

    systemd_logger: Don't generate build warnings when all is OK
    libsystemd-journal is only found with old versions of systemd - check it
    is installed before querying.

--- plugins/systemd_logger/uwsgiplugin.py
+++ plugins/systemd_logger/uwsgiplugin.py
@@ -2,9 +2,13 @@ import os
 
 NAME = 'systemd_logger'
 
-CFLAGS = os.popen('pkg-config --cflags libsystemd-journal').read().rstrip().split()
-CFLAGS += os.popen('pkg-config --cflags libsystemd').read().rstrip().split()
+if os.popen('pkg-config --exists libsystemd-journal').close() is None:
+	CFLAGS = os.popen('pkg-config --cflags libsystemd-journal').read().rstrip().split()
+	LIBS = os.popen('pkg-config --libs libsystemd-journal').read().rstrip().split()
+else:
+	CFLAGS = os.popen('pkg-config --cflags libsystemd').read().rstrip().split()
+	LIBS = os.popen('pkg-config --libs libsystemd').read().rstrip().split()
+
 LDFLAGS = []
-LIBS = os.popen('pkg-config --libs libsystemd-journal').read().rstrip().split()
-LIBS += os.popen('pkg-config --libs libsystemd').read().rstrip().split()
+
 GCC_LIST = ['systemd_logger']
