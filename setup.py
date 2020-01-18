daa7f156d836582ec5934c71a7bef57b27ce3fff
Hugo <hugovk@users.noreply.github.com>
2020-01-19 00:47:24 +0200

    Add Trove classifiers for Python 3.7 and 3.8

--- setup.py
+++ setup.py
@@ -136,5 +136,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
