3969f67735e9895eb255b9bc8db882772f09775c
Daniel Holth <dholth@fastmail.fm>
2020-05-16 17:13:22 -0400

    add greenlets

--- plugins/cffi/cffi_dyn_init.py
+++ plugins/cffi/cffi_dyn_init.py
@@ -14,10 +14,7 @@ def uwsgi_cffi_after_request(wsgi_req):
 import cffi_asyncio
 
 
-"""
-Continulets support
-"""
-
+# Continulets support
 # see also very similar uwsgi_pypy_greenlets.py
 # which has the advantage of being able to run on CPython
 
@@ -78,3 +75,67 @@ def uwsgi_pypy_setup_continulets():
     lib.uwsgi.schedule_to_main = lib.uwsgi_pypy_continulet_switch
     lib.uwsgi.schedule_to_req = lib.uwsgi_pypy_continulet_schedule
     print("*** PyPy Continulets engine loaded ***")
+
+
+# Greenlets support
+# May prefer to continulets.
+# Built in to pypy (built on top of continulets)
+
+uwsgi_pypy_greenlets = {}
+
+
+def uwsgi_pypy_greenlet_wrapper():
+    lib.async_schedule_to_req_green()
+
+
+@ffi.def_extern()
+def uwsgi_pypy_greenlet_schedule():
+    id = lib.uwsgi.wsgi_req.async_id
+    modifier1 = lib.uwsgi.wsgi_req.uh.modifier1
+
+    # generate a new greenlet
+    if not lib.uwsgi.wsgi_req.suspended:
+        uwsgi_pypy_greenlets[id] = greenlet.greenlet(uwsgi_pypy_greenlet_wrapper)
+        lib.uwsgi.wsgi_req.suspended = 1
+
+    # this is called in the main stack
+    if lib.uwsgi.p[modifier1].suspend:
+        lib.uwsgi.p[modifier1].suspend(ffi.NULL)
+
+    # let's switch
+    uwsgi_pypy_greenlets[id].switch()
+
+    # back to the main stack
+    if lib.uwsgi.p[modifier1].resume:
+        lib.uwsgi.p[modifier1].resume(ffi.NULL)
+
+
+@ffi.def_extern()
+def uwsgi_pypy_greenlet_switch(wsgi_req):
+    wsgi_req.async_id
+    modifier1 = wsgi_req.uh.modifier1
+
+    # this is called in the current greenlet
+    if lib.uwsgi.p[modifier1].suspend:
+        lib.uwsgi.p[modifier1].suspend(wsgi_req)
+
+    uwsgi_pypy_main_greenlet.switch()
+
+    # back to the greenlet
+    if lib.uwsgi.p[modifier1].resume:
+        lib.uwsgi.p[modifier1].resume(wsgi_req)
+
+    # update current running greenlet
+    lib.uwsgi.wsgi_req = wsgi_req
+
+
+def uwsgi_cffi_setup_greenlets():
+    global greenlet, uwsgi_pypy_main_greenlet
+    import greenlet
+
+    if getattr(lib.uwsgi, "async") < 1:
+        raise Exception("pypy greenlets require async mode !!!")
+
+    lib.uwsgi.schedule_to_main = lib.uwsgi_pypy_greenlet_switch
+    lib.uwsgi.schedule_to_req = lib.uwsgi_pypy_greenlet_schedule
+    uwsgi_pypy_main_greenlet = greenlet.getcurrent()
