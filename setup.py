a04820f092aafc8a8807769fe179757ee8337b5b
Adrian <adrian@planetcoding.net>
2021-04-26 22:36:23 +0200

    Add trove classifier for Python 3.9

--- setup.py
+++ setup.py
@@ -146,6 +146,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     install_requires=get_extra_require()
 )
