c228254a1dfcdb0f526629a4f6b142001b8b11ee
Joe <cuchac@email.cz>
2022-07-09 03:52:27 +0200

    PHP: Fix php-app for php81

--- plugins/php/php_plugin.c
+++ plugins/php/php_plugin.c
@@ -817,6 +817,7 @@ int uwsgi_php_request(struct wsgi_request *wsgi_req) {
 #endif
 
 		strcpy(real_filename, uphp.app);	
+		real_filename_len = strlen(real_filename);
 		if (wsgi_req->path_info_len == 1 && wsgi_req->path_info[0] == '/') {
 			goto appready;
 		}
