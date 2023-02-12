a123e01a23b3f6876cfeefee08d87d9ef1ac548a
Evengard <evengard@trioptimum.ru>
2023-02-12 06:51:25 +0300

    The mono plugin should be linked against the C++ standard library to work

--- plugins/mono/uwsgiplugin.py
+++ plugins/mono/uwsgiplugin.py
@@ -4,7 +4,7 @@ import subprocess
 NAME = 'mono'
 
 CFLAGS = os.popen('pkg-config --cflags mono-2').read().rstrip().split()
-LDFLAGS = []
+LDFLAGS = ['-lstdc++']
 LIBS = os.popen('pkg-config --libs mono-2').read().rstrip().split()
 GCC_LIST = ['mono_plugin']
 
