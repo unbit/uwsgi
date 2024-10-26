bcde41f310eab82abaeb98369bfca47ea61b5be3
Lalufu <Lalufu@users.noreply.github.com>
2024-10-26 11:13:54 +0200

    Fix reload-on-touch/change (#2689)
    * Fix reload-on-touch/change
    
    As part of a fix for #2656, commit 8f1d0e5 introduced a measure to wait
    for processes to finish work while reloading. The code to do this
    affects other logic flows was well, breaking the ability to reload uwsgi
    when a config file changes.
    
    This moves the code around a bit so #2656 stays fixed, but
    reload-on-touch/change works again.
    
    Fixes #2681.
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>

--- core/master_utils.c
+++ core/master_utils.c
@@ -54,12 +54,6 @@ void uwsgi_destroy_processes() {
 
         uwsgi_detach_daemons();
 
-	for (i = 1; i <= uwsgi.numproc; i++) {
-		if (uwsgi.workers[i].pid > 0) {
-			waitpid(uwsgi.workers[i].pid, &waitpid_status, 0);
-		}
-	}
-
         for (i = 0; i < ushared->gateways_cnt; i++) {
                 if (ushared->gateways[i].pid > 0) {
                         kill(ushared->gateways[i].pid, SIGKILL);
--- core/uwsgi.c
+++ core/uwsgi.c
@@ -1319,6 +1319,8 @@ void kill_them_all(int signum) {
 // gracefully destroy
 void gracefully_kill_them_all(int signum) {
 
+        int waitpid_status;
+
         if (uwsgi_instance_is_dying) return;
         uwsgi.status.gracefully_destroying = 1;
 
@@ -1339,6 +1341,12 @@ void gracefully_kill_them_all(int signum) {
                 if (uwsgi.mules[i].pid > 0) {
                         uwsgi_curse_mule(i, SIGHUP);
                 }
+        }
+
+        for (i = 1; i <= uwsgi.numproc; i++) {
+            if (uwsgi.workers[i].pid > 0) {
+                waitpid(uwsgi.workers[i].pid, &waitpid_status, 0);
+            }
         }
 
         uwsgi_destroy_processes();
